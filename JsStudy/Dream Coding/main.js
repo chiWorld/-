c4a7cce7063ade84550e86bd53ec4b0a1306b129
chiWorld <fellyoul7@gmail.com>
2022-06-27 22:42:42 +0900

    드림코딩 js공부

---
+++ JsStudy/Dream Coding/main.js
@@ -0,0 +1,16 @@
+/**
+ * - 자바스크립트 공식사이트 : https://www.ecma-international.org/
+ *                            https://developer.mozilla.org/ko/
+ * 
+ * - API (Application Programming Interface)
+ * 
+ * - Web API : 브라우저가 이해할 수 있는 함수들
+ */
+
+//ts를 할 때는 선언 필요 없음
+//ECMAScript5에 선언되어 있는 것
+//  - vanilly js를 사용할 때 이걸 선언하면 상식적 범위 내에서 js 이용할 수 있게 함
+//  - js엔진이 좀 더 빠르게 동작하게 함
+'use strict';
+
+console.log("Hello world!"); //Console API를 통해서 출력하는 것
